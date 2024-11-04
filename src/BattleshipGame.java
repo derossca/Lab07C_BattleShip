2eaac0f667afc963bc84c8e64f01c1c7acef4482
derossca <derossca@mail.uc.edu>
2024-11-04 11:40:45 -0500

    set up fields and methods for BattleshipGame class

--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -1,2 +1,40 @@
 public class BattleshipGame {
+
+    private GameBoard gameBoard;
+    private int totalHits;
+    private int totalMisses;
+    private int currentMisses;
+    private int strikes;
+
+    public BattleshipGame() {
+
+    }
+
+    public boolean fireMissile(int x, int y) {
+
+    }
+
+    public boolean allShipsSunk() {
+
+    }
+
+    public boolean isGameOver() {
+
+    }
+
+    public int getTotalHits() {
+
+    }
+
+    public int getTotalMisses() {
+
+    }
+
+    public int getCurrentMisses() {
+
+    }
+
+    public int getStrikes() {
+
+    }
 }
