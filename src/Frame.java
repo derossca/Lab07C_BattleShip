18c623bb21e0aeca1ec55fbebd997b6e5f400ad7
derossca <derossca@mail.uc.edu>
2024-11-04 11:45:30 -0500

    set up fields and methods for Frame class

--- src/Frame.java
+++ src/Frame.java
@@ -1,2 +1,46 @@
-public class Frame {
+import javax.swing.*;
+
+public class Frame extends JFrame {
+    private final int BOARD_SIZE = 10;
+    private JButton[][] boardButtons;
+    private BattleshipGame game;
+
+    private JLabel totalHitLabel;
+    private JLabel totalMissLabel;
+    private JLabel currentMissLabel;
+    private JLabel strikesLabel;
+
+    public Frame() {
+
+    }
+
+    private void initializeBoard(JPanel boardPanel) {
+
+    }
+
+    private void initializeStatusPanel(JPanel statusPanel) {
+
+    }
+
+    private void fireMissile(int x, int y, JButton button) {
+
+    }
+
+    private void updateStatus() {
+
+    }
+
+    private void playAgain() {
+
+    }
+
+    private void quitGame() {
+
+    }
+
+    private void resetGame() {
+
+    }
+
+
 }
