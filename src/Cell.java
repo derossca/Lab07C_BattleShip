3c572fd26c02e12a8117ac4ff318dad4b8a42238
derossca <derossca@mail.uc.edu>
2024-11-04 11:32:06 -0500

    set up fields and methods for Cell class

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,25 @@
 public class Cell {
+
+    private String display;
+    private boolean hasShip;
+
+    public Cell(){
+
+    }
+
+    public String getDisplay() {
+
+    }
+
+    public void setDisplay(String display) {
+
+    }
+
+    public boolean hasShip() {
+
+    }
+
+    public void placeShip() {
+
+    }
 }
