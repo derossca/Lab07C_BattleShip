b47db84c66b63b28584f69c716dd144d9337927a
derossca <derossca@mail.uc.edu>
2024-11-04 11:33:29 -0500

    set up fields and methods for Ship class

--- src/Ship.java
+++ src/Ship.java
@@ -1,2 +1,22 @@
 public class Ship {
+
+    private int size;
+    private boolean isSunk;
+    private int hitCount;
+
+    public Ship(int size) {
+
+    }
+
+    public int getSize() {
+
+    }
+
+    public boolean isSunk() {
+
+    }
+
+    public void hit() {
+
+    }
 }
