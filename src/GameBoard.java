6f35a4f293616e79bbd77d0752ae008c7d7fd516
derossca <derossca@mail.uc.edu>
2024-11-04 11:37:37 -0500

    set up fields and methods for GameBoard class

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,2 +1,36 @@
+import java.util.List;
+
 public class GameBoard {
+
+    private final int BOARD_SIZE = 10;
+    private Cell[][] cells;
+    private List<Ship> ships;
+
+    public GameBoard() {
+
+    }
+
+    public Cell getCell(int x, int y) {
+
+    }
+
+    public boolean checkHit(int x, int y) {
+
+    }
+
+    public void placeShips() {
+
+    }
+
+    public boolean canPlaceShip(int size, int row, int col, boolean vertical) {
+
+    }
+
+    public void placeShip(int size, int row, int col, boolean vertical) {
+
+    }
+
+    public List<Ship> getShips() {
+
+    }
 }
